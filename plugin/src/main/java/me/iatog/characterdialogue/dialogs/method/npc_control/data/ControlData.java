188e8592fc76b7953ec8b12666d5481f1cd405a8
iAtog <atogmc58vani@gmail.com>
2024-12-26 03:23:55 -0400

    feat(plugin): expand ControlData

--- plugin/src/main/java/me/iatog/characterdialogue/dialogs/method/npc_control/data/ControlData.java
+++ plugin/src/main/java/me/iatog/characterdialogue/dialogs/method/npc_control/data/ControlData.java
@@ -2,6 +2,31 @@ package me.iatog.characterdialogue.dialogs.method.npc_control.data;
 
 import me.iatog.characterdialogue.adapter.AdaptedNPC;
 
-public record ControlData(AdaptedNPC getOriginal, AdaptedNPC getCopy) {
+public class ControlData {
+    private final AdaptedNPC original;
+    private final AdaptedNPC copy;
 
+    private boolean hideOriginal;
+
+    public ControlData(AdaptedNPC original, AdaptedNPC copy) {
+        this.original = original;
+        this.copy = copy;
+    }
+
+
+    public AdaptedNPC getOriginal() {
+        return original;
+    }
+
+    public AdaptedNPC getCopy() {
+        return copy;
+    }
+
+    public void setHideOriginal(boolean hideOriginal) {
+        this.hideOriginal = hideOriginal;
+    }
+
+    public boolean isHideOriginal() {
+        return hideOriginal;
+    }
 }
