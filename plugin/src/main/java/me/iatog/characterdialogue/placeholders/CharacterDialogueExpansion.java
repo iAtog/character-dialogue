a24b8e9a3bf6f7f23657fe5e9ccd1f1f8199de14
iAtog <atogmc58vani@gmail.com>
2024-12-31 16:37:38 -0400

    feat(expansion): added %characterdialogue_has_item:<item>:<amount>% expansion

--- plugin/src/main/java/me/iatog/characterdialogue/placeholders/CharacterDialogueExpansion.java
+++ plugin/src/main/java/me/iatog/characterdialogue/placeholders/CharacterDialogueExpansion.java
@@ -2,8 +2,10 @@ package me.iatog.characterdialogue.placeholders;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
+import me.iatog.characterdialogue.libraries.ItemManager;
 import me.iatog.characterdialogue.player.PlayerData;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -68,9 +70,42 @@ public class CharacterDialogueExpansion extends PlaceholderExpansion {
             }
 
             return String.valueOf(list.size());
+        } else if(params.startsWith("has_item")) { // %characterdialogue_has_item:itemName:amount%
+            String[] split = params.split(":");
+            ItemManager manager = main.getServices().getItemManager();
+
+            if(split.length != 3 || !manager.existsItem(split[1]))
+                return "undefined";
+
+            String itemName = split[1];
+            int amount = parseInt(split[2]);
+
+            return hasItemAmount(player, manager.getItem(itemName), amount) ? "yes" : "no";
         }
 
         return null;
+    }
+
+    private boolean hasItemAmount(Player player, ItemStack itemStack, int amount) {
+        int totalAmount = 0;
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null && item.isSimilar(itemStack)) {
+                totalAmount += item.getAmount();
+                if (totalAmount >= amount) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private int parseInt(String integer) {
+        try {
+            return Integer.parseInt(integer);
+        } catch(NumberFormatException ex) {
+            return 1;
+        }
     }
 
 }
