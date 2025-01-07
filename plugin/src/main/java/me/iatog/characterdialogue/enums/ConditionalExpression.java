bb41906d65f675d319cc7749b71a9b0f61229276
iAtog <atogmc58vani@gmail.com>
2025-01-07 08:56:10 -0400

    feat: use minimessage in stop_send_msg in ConditionalMethod

--- plugin/src/main/java/me/iatog/characterdialogue/enums/ConditionalExpression.java
+++ plugin/src/main/java/me/iatog/characterdialogue/enums/ConditionalExpression.java
@@ -1,12 +1,11 @@
 package me.iatog.characterdialogue.enums;
 
 import me.iatog.characterdialogue.dialogs.method.conditional.ConditionData;
-import me.iatog.characterdialogue.placeholders.Placeholders;
 import me.iatog.characterdialogue.session.DialogSession;
 import me.iatog.characterdialogue.session.EmptyDialogSession;
+import me.iatog.characterdialogue.util.AdventureUtil;
 import me.iatog.characterdialogue.util.SingleUseConsumer;
 import me.iatog.characterdialogue.util.TextUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.Collections;
@@ -19,10 +18,9 @@ public enum ConditionalExpression {
         DialogSession session = data.getSession();
         Player player = session.getPlayer();
         String expression = data.getExpression();
-        //session.destroy();
         completed.accept(CompletedType.DESTROY);
 
-        if (! data.getMain().getCache().getDialogues().containsKey(expression) || expression.isEmpty()) {
+        if (!data.getMain().getCache().getDialogues().containsKey(expression) || expression.isEmpty()) {
             data.getMain().getLogger().severe("The dialogue '" + expression + "' was not found.");
             player.sendMessage(TextUtils.colorize("&c&lUnknown dialogue found."));
             return;
@@ -33,14 +31,13 @@ public enum ConditionalExpression {
     STOP_SEND_MSG((data, completed) -> {
         Player player = data.getSession().getPlayer();
         completed.accept(CompletedType.DESTROY);
-        player.sendMessage(Placeholders.translate(player, data.getExpression()
-              .replace(data.getSession().getDisplayName(), ChatColor.stripColor(data.getSession().getDisplayName()))));
+        AdventureUtil.sendMessage(player, data.getExpression()
+              .replace("%npc_name%", data.getSession().getDisplayName()));
     }),
-    STOP((data, completed) -> {
-        completed.accept(CompletedType.DESTROY);
-    }),
+
+    STOP((data, completed) -> completed.accept(CompletedType.DESTROY)),
+
     RUN_METHOD((data, completed) -> {
-        //completed.accept(CompletedType.PAUSE);
         DialogSession session = data.getSession();
         Player player = session.getPlayer();
         String expression = data.getExpression();
@@ -48,20 +45,18 @@ public enum ConditionalExpression {
         if (data.getExpression().isEmpty() || data.getMain().getCache().getMethods().containsKey(expression)) {
             session.destroy();
             data.getMain().getLogger().severe("The dialogue '" + expression + "' was not found.");
-            player.sendMessage("&c&lUnknown method found.");
+            AdventureUtil.sendMessage(player, "<red><bold>Unknown method found: " + expression);
             return;
         }
 
         data.getMain().getApi().runDialogueExpression(player, expression, session.getDisplayName(),
-              SingleUseConsumer.create(completedRes -> {
-                  completed.accept(CompletedType.CONTINUE);
-              }), new EmptyDialogSession(data.getMain(), player, Collections.singletonList(expression), session.getDisplayName(),
+              SingleUseConsumer.create(completedRes ->
+                  completed.accept(CompletedType.CONTINUE)
+              ), new EmptyDialogSession(data.getMain(), player, Collections.singletonList(expression), session.getDisplayName(),
                     data.getSession().getNPC()), data.getSession().getNPC());
 
     }),
-    CONTINUE((data, completed) -> {
-        completed.accept(CompletedType.CONTINUE);
-    });
+    CONTINUE((data, completed) -> completed.accept(CompletedType.CONTINUE));
 
     private final BiConsumer<ConditionData, Consumer<CompletedType>> action;
 
