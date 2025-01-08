a792f910980a6bc485f9c523abbc2ea33913fce2
iAtog <atogmc58vani@gmail.com>
2025-01-07 20:57:34 -0400

    chore: more adjustments

--- plugin/src/main/java/me/iatog/characterdialogue/dialogs/method/SendMethod.java
+++ plugin/src/main/java/me/iatog/characterdialogue/dialogs/method/SendMethod.java
@@ -3,7 +3,9 @@ package me.iatog.characterdialogue.dialogs.method;
 import me.iatog.characterdialogue.CharacterDialoguePlugin;
 import me.iatog.characterdialogue.dialogs.DialogMethod;
 import me.iatog.characterdialogue.dialogs.MethodContext;
+import me.iatog.characterdialogue.placeholders.Placeholders;
 import me.iatog.characterdialogue.util.AdventureUtil;
+import org.bukkit.entity.Player;
 
 public class SendMethod extends DialogMethod<CharacterDialoguePlugin> {
 
@@ -15,10 +17,12 @@ public class SendMethod extends DialogMethod<CharacterDialoguePlugin> {
 
     @Override
     public void execute(MethodContext context) {
+        Player player = context.getPlayer();
+
         AdventureUtil.sendMessage(
               context.getPlayer(),
-              context.getConfiguration().getArgument(),
-              AdventureUtil.placeholder("player", context.getPlayer().getName())
+              Placeholders.translate(player, context.getConfiguration().getArgument()),
+              AdventureUtil.placeholder("player", player.getName())
         );
 
         context.next();
